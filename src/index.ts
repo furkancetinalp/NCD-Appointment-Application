ba515a0d8a5d9f7b17f40958ddbcb3139ddbd846
Furkan Cetinalp <cetinalp.furkan0@gmail.com>
2022-04-20 22:00:29 +0300

    commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import {appointment} from "./model";
+
+export function appoint(name:string,age:u32,day:u32):string{
+  const a = new appointment(name,age,day);
+  return a.register();
+}
+
+
+export function information():appointment{
+  return appointment.info();
+}
+
+export function changeDay(day:u32):appointment{
+  return appointment.changeDay(day);
+}
+
+export function del():string{
+  return appointment.delete_account();
+}
